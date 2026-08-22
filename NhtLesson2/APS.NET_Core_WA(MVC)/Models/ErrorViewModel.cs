d40610f95faf1b96d4281889fc3403334484cc26
thanhhngu <thanhhhngu.06@gmail.com>
2026-08-22 16:38:40 +0700

    Remove folder NhtLesson2

--- NhtLesson2/APS.NET_Core_WA(MVC)/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace APS.NET_Core_WA_MVC_.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
